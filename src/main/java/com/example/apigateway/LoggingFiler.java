77fc7b211cacc4503decf0bd6a2dc1f20b2772ba
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 <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>
2024-03-12 21:07:45 +0530

    loggers and authentication service

---
+++ src/main/java/com/example/apigateway/LoggingFiler.java
@@ -0,0 +1,22 @@
+package com.example.apigateway;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+
+@Component
+public class LoggingFiler implements GlobalFilter {
+
+    private Logger logger= LoggerFactory.getLogger(LoggingFiler.class);
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        logger.info("Path of request -> {}",exchange.getRequest().getPath());
+        return chain.filter(exchange);
+
+    }
+}
